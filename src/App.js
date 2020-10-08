b95e36c1a8e8104b55a2404eb904c584d390fff4
Pankaj Boruah <pankaj.boruah@upgrad.com>
2020-10-08 13:52:43 +0530

    details page using reach router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 // import Pet from "./pet";
 const App = () => {
 	return (
 		<div id="container">
-			<h1 id="title">Adopt Here!</h1>
-			<SearchParams />
+			<header>
+				<Link to="/">Adopt Here</Link>
+			</header>
+			<Router>
+				<SearchParams path="/" />
+				<Details path="/details/:id" />
+			</Router>
 		</div>
 	);
 };
